4493fafa4ba2e29a905b76ab4c8bc73f5f1c2261
y6yuan <y6yuan@gmail.com>
2020-07-06 15:57:57 +0800

    Initial commit

---
+++ app/src/main/java/me/y6yuan/v2master/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package me.y6yuan.v2master.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
